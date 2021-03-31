c88c7e036d2c741ce1bff28f0adedce5ef47e80f
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-03-31 09:54:15 +0000

    students sorted randomly for one group

--- file.js
+++ file.js
@@ -7,4 +7,12 @@ var students = ["Maxime", "Juan", "Yohan", "Laurène", "Valentin", "Fanny",
 var student = students[Math.floor(Math.random() * students.length)];
 console.log(student);
 
-//à présent je veux placer 7 élèves dans un groupe et 8 dans l'autre
+//je veux placer 7 élèves dans un groupe et 8 dans l'autre
+for (let i=0; i<7; i++){
+	var result = Math.floor(Math.random() * students.length);
+	//je pousse dans "group1" le nom d'un élève obtenu aléatoirement dans le tableau "students"
+	group1.push(students[result]);
+	//j'évite d'avoir un nom qui revient plusieurs fois dans le tableau
+	students.splice(result, 1);
+}
+console.log(group1);
