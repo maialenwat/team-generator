98343573bf24cd9fa2c6a55ca6abc8e13a82cc78
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-04-01 10:37:16 +0000

    table changed in html file, function in progress in js

--- file.js
+++ file.js
@@ -6,32 +6,23 @@ var students = ["Maxime", "Juan", "Yohan", "Laurène", "Valentin", "Fanny",
 var group1 = [];
 
 
-//je veux placer 7 élèves dans un groupe et 8 dans l'autre
-for (let i=0; i<7; i++){
+//je récupère mon bouton cliquable via son ID
+var btn = document.getElementById("button-addon1");
+
+btn.addEventListener("click", ()=>{
+	
+	//je veux placer 7 élèves dans un groupe et 8 dans l'autre
+	for (let i=0; i<7; i++){
 	var result = Math.floor(Math.random() * students.length);
 	//je pousse dans "group1" le nom d'un élève obtenu aléatoirement dans le tableau "students"
 	group1.push(students[result]);
 	//j'évite d'avoir un nom qui revient plusieurs fois dans le tableau
 	students.splice(result, 1);
-}
-
-//ce qu'il reste du tableau "students" est donc le "group2"
-var group2 = students;
+	}
+	//ce qu'il reste du tableau "students" est donc le "group2"
+	var group2 = students;
+})
 
 console.log(group1);
 console.log(group2);
-
-
-//TO DO LIST :
-//en manipulant le DOM animer le bouton cliquable
-//afficher dans un tableau les noms d'élèves générés par l'algo
-
-//je récupère mon bouton
-var btn = document.getElementById("button-addon1");
-
-btn.addEventListener("click", ()=>{
-	
-})
-
-//je récupère mon tableau
 
