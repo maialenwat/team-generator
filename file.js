41b7518e355c69381c1bec0423a85fedc1126399
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-04-01 15:08:10 +0000

    var temp added

--- file.js
+++ file.js
@@ -3,24 +3,33 @@ var students = ["Maxime", "Juan", "Yohan", "Laurène", "Valentin", "Fanny",
 "Nourredine", "Marylise", "Raphaël", "Alexandre", "Tamara", "Maïalen", "Lucas",
 "Sidney", "Vincent"];
 
+//j'initialise une variable temporaire qui stocke tous les élèves
+var temp = students;
+
 var group1 = [];
 
 document.getElementById("button-addon1").addEventListener("click", ()=>{
+
+	//je pointe mes balises <ul> via leur id
+	var zone1 = document.getElementById("g1");
+	var zone2 = document.getElementById("g2");
+
+	//je réinitialise le tableau à chaque nouveau click
+	zone1.innerHTML = '';
+	zone2.innerHTML = '';
+
 	//je veux placer 7 élèves dans un groupe et 8 dans l'autre
 	for (let i=0; i<7; i++){
-		var result = Math.floor(Math.random() * students.length);
+		var result = Math.floor(Math.random() * temp.length);
 		//je pousse dans "group1" le nom d'un élève obtenu aléatoirement dans le tableau "students"
-		group1.push(students[result]);
+		group1.push(temp[result]);
 		//j'évite d'avoir un nom qui revient plusieurs fois dans le tableau
-		students.splice(result, 1);
+		temp.splice(result, 1);
 	}
 
-	//ce qu'il reste du tableau "students" est donc le "group2"
-	var group2 = students;
+	//ce qu'il reste du tableau "temp" est donc le "group2"
+	var group2 = temp;
 
-
-	//je pointe ma balise <ul> via son id
-	var zone1 = document.getElementById("g1");
 
 	for (let i = 0; i < group1.length; i++){
 		var element = document.createElement("li");
@@ -28,8 +37,6 @@ document.getElementById("button-addon1").addEventListener("click", ()=>{
 		zone1.appendChild(element);
 	}
 	
-
-	var zone2 = document.getElementById("g2");
 
 	for (let j = 0; j < group2.length; j++){
 		var element = document.createElement("li");
@@ -39,8 +46,6 @@ document.getElementById("button-addon1").addEventListener("click", ()=>{
 
 });
 
-zone1.innerHTML = ('');
-zone2.innerHTML = ('');
 
 
 
